76f1bfb2bdc2eb051277f524322e37b775855e1d
Sarthak Mishra <155018781+Shaivarth@users.noreply.github.com>
2025-02-12 02:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io(https://livechat-testing.onrender.com);
+const socket = io("https://livechat-testing.onrender.com");
 let username = "";
 
 // Function to generate a unique color based on the username
