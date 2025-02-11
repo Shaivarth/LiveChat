9122a228c7b12404c934b0eab76f036aa4ca4609
Sarthak Mishra <155018781+Shaivarth@users.noreply.github.com>
2025-02-12 01:50:44 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+    console.log("New user connected");
+
+    socket.on("message", (data) => {
+        io.emit("message", data); // Broadcast message with username
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
